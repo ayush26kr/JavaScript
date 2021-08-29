f1c6648d40345743b84c5b956d75c10f742308eb
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-08-30 01:30:24 +0530

    Create love calculator.js

---
+++ love calculator.js
@@ -0,0 +1,12 @@
+prompt("What is your name? ");
+prompt("What is their name?");
+var loveScore=Math.random()*100;  //0-99
+loveScore=Math.floor(loveScore)+1;  //1-100
+
+if(loveScore==100)
+{
+  alert("Your score is "+loveScore+"% You are Amazing!!");
+}
+else{
+  alert("Your score is "+loveScore+"% ");
+}
