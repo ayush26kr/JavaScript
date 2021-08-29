1cda14106e4ccee77f58aebdeeb55b9067065dfd
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-08-29 22:56:18 +0530

    Create string slice.js

---
+++ string slice.js
@@ -0,0 +1,6 @@
+//var_name.slce(a,b);    a inclusive and b exclusive
+var tweet=prompt("Enter your Tweet :");
+var tweetUnder140=tweet.slice(0,140);
+alert(tweetUnder140);
+
+//alert(prompt("Enter your Tweet :").slice(0,140));
