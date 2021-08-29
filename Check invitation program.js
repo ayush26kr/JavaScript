56d1b9257ce23d48a59a71dd713937f446d3712b
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-08-30 02:15:56 +0530

    Use of array

---
+++ Check invitation program.js
@@ -0,0 +1,8 @@
+var guestList=["Angel","Jack","Ron"];
+var guestName=prompt("Enter your name :");
+if(guestList.includes(guestName))
+{
+    alert("Welcome ! To the Party.")
+}
+else
+alert("Sorry! Not Invited.");
